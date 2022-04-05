7e288ee2cff3b945bca173384a09af468debc49c
d0rf <dorianvoets@gmail.com>
2022-04-05 12:19:51 +0200

    First commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "google" {
+  project = var.gcp_project
+  region  = var.region
+}
+
+resource "random_password" "password" {
+  // Create a new password with special chars and 16 characters
+  length  = 16
+  special = true
+}
+
+resource "google_sql_database_instance" "master" {
+  // Create a new sql database with variables.tf content
+  # project             = var.gcp_project
+  name             = var.instance_name
+  database_version = var.database_version
+  # region              = var.region
+  deletion_protection = "false"
+  // We allow internet access only for lab purpose
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled = true
+      authorized_networks {
+        name  = "internet"
+        value = "0.0.0.0/0"
+      }
+    }
+  }
+}
+
+resource "google_sql_user" "users" {
+  // Create the database user
+  # project  = var.gcp_project
+  name     = var.username
+  instance = google_sql_database_instance.master.name
+  password = random_password.password.result
+}
+
+resource "vault_generic_secret" "example" {
+  // Put the password in vault
+  path = "secret/${google_sql_user.users.name}"
+  //data_json = jsonencode(random_password.password)
+  data_json = <<EOT
+{
+  "user":   "${google_sql_user.users.name}",
+  "password":   "${random_password.password.result}"
+}
+EOT
+}
